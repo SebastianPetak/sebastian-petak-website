efa74905047df1e791d5943251e68fab529ce8b1
Sebastian Petak <sebastianpetak@gmail.com>
2017-05-09 18:45:26 -0500

    Fix mobile nav, fix typo Mobile navigation links don't close the dropdown when selected. Make the dropdown close after a mobile link is selected. Write Node as Node.js in current Projects.

--- views/js/app.js
+++ views/js/app.js
@@ -1,6 +1,12 @@
 $(document).foundation();
 
 $(document).ready(function() {
+
+	/* Automatically close mobile nav menu when a link is clicked */
+	$('.mobile-link').on('click',function() {
+		$('#mobile-nav-toggle').click();
+	});
+
 	/* Object contains each projects information */
 	const projectsObject = {
 		fccLeaderboard: {
